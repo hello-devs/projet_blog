97bbbc853bdf89cdee70d69e8e8847d7dd1aa6c1
hello-devs <30301241+hello-devs@users.noreply.github.com>
2018-03-31 11:09:13 +0200

    update PostM and CommentM

--- model/PostManager.php
+++ model/PostManager.php
@@ -3,5 +3,77 @@
 
 class PostManager extends Manager
 {
+    public function getPosts()  //A remplacer par getPostss($etat)
+    {
+        $db = $this->dbConnect();
+        $req = $db->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr, etat FROM posts ORDER BY creation_date DESC');
+
+        return $req;
+    }
+
+    public function getPostss($etat)
+    {
+        switch($etat)
+        {
+            case ('all'):
+                $sql = 'SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr, etat FROM posts ORDER BY creation_date DESC';
+                break;
+
+            case ('publié'):
+                $sql = 'SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr, etat FROM posts WHERE etat = "publié" ORDER BY creation_date DESC';
+                break;
+
+            case ('brouillon'):
+                $sql = 'SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr, etat FROM posts WHERE etat = "brouillon" ORDER BY creation_date DESC';
+                break;
+
+            default:
+                throw new Exception("Ce cas n'est pas prévu par le développeur dans sa fonction getPosts() !");
+        }
+
+
+
+
+
+
+        $db = $this->dbConnect();
+        $req = $db->query($sql);
+
+        return $req;
+    }
+
+    public function getPost($postId)
+    {
+        $db = $this->dbConnect();
+        $req = $db->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr, etat FROM posts WHERE id = ?');
+        $req->execute(array($postId));
+        $post = $req->fetch();
+
+        return $post;
+    }
+
+    public function editPost($postId)
+    {
+        $db = $this->dbConnect();
+        $editPost = $db->prepare('
+        Update posts
+        SET title = :title,
+        content = :content,
+        WHERE id = :postId');
+
+        $editPost->execute(array(
+        'content' => $_POST['content'],
+        'postId' => $postId));
+    }
+
+    public function deletePost($postID)
+    {
+        $db = $this->dbConnect();
+        $delPost = $db->exec('
+            DELETE FROM posts
+            WHERE id ='.$postID);
+    }
+
+
 
 }
