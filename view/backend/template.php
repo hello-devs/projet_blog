29dc2712049d835d377686b2f7810078bc2ddd6f
hello-devs <30301241+hello-devs@users.noreply.github.com>
2018-05-17 11:37:47 +0200

    style

--- view/backend/template.php
+++ view/backend/template.php
@@ -24,7 +24,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 </head>
 
-<body class="grey lighten-2">
+<body class="grey">
     <div class="">
         <div class="row bandeau">
             <p class="bandeau card center s12">Espace d' administration du Blog</p>
--- view/frontend/template.php
+++ view/frontend/template.php
@@ -20,7 +20,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 </head>
 
-<body class="grey lighten-5">
+<body class="grey">
 
     <div class="row">
         <header class="">
