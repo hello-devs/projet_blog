71319e0449f9fca43d4f80b93d7fa5113c87a9af
hello-devs <30301241+hello-devs@users.noreply.github.com>
2018-08-28 15:44:30 +0200

    pas d'indexation <meta>

--- view/backend/template.php
+++ view/backend/template.php
@@ -1,54 +1,56 @@
 <!DOCTYPE html>
 <html lang="fr">
 
-<head>
-    <title>
-        <?= $title ?>
-    </title>
+    <head>
+        <title>
+            <?= $title ?>
+        </title>
 
+        <meta name="robots" content="noindex">
+        <meta name="googlebot" content="noindex">
 
-    <!-- Compiled and minified MAterialize CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+        <!-- Compiled and minified MAterialize CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
 
-    <!--     Css     -->
-    <link rel="stylesheet" type="text/css" href="public/css/style.css" />
+        <!--     Css     -->
+        <link rel="stylesheet" type="text/css" href="public/css/style.css" />
 
-    <!--Icons-->
-    <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js" integrity="sha384-SlE991lGASHoBfWbelyBPLsUlwY1GwNDJo3jSJO04KZ33K2bwfV9YBauFfnzvynJ" crossorigin="anonymous"></script>
+        <!--Icons-->
+        <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js" integrity="sha384-SlE991lGASHoBfWbelyBPLsUlwY1GwNDJo3jSJO04KZ33K2bwfV9YBauFfnzvynJ" crossorigin="anonymous"></script>
 
-    <!--TyniMce-->
-    <script src="public/js/tinymce/tinymce.min.js"></script>
-    <script>tinymce.init({ selector:'textarea' , language : "fr_FR"});</script>
+        <!--TyniMce-->
+        <script src="public/js/tinymce/tinymce.min.js"></script>
+        <script>tinymce.init({selector: 'textarea', language: "fr_FR"});</script>
 
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-</head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    </head>
 
-<body class="grey lighten-4">
-    <div class="">
-        <div class="row bandeau">
-            <p class="bandeau card center s12">Espace d' administration du Blog</p>
+    <body class="grey lighten-4">
+        <div class="">
+            <div class="row bandeau">
+                <p class="bandeau card center s12">Espace d' administration du Blog</p>
+            </div>
+
+            <div class="row">
+                <header class="s12">
+                    <nav class="navbar-fixed blue-grey darken-4">
+                        <ul class="navbar-nav">
+                            <li><a href="?action=manageBlog">Tableau de bord</a></li>
+                            <li><a href="?action=manageArticles">Gestion des articles</a></li>
+                            <li><a href="?action=manageComs">Gestion des commentaires</a></li>
+                            <li><a href="?action=listPosts" target="_blank">Retour au site</a></li>
+
+                        </ul>
+                        <ul class="navbar-nav right">
+                            <li class="left"><a href="?action=decoAdmin"><?= ucfirst($_SESSION['user']->logName()) ?> (se déconnecter)</a></li>
+                        </ul>
+                    </nav>
+                </header>
+            </div>
         </div>
 
-        <div class="row">
-            <header class="s12">
-                <nav class="navbar-fixed blue-grey darken-4">
-                    <ul class="navbar-nav">
-                        <li><a href="?action=manageBlog">Tableau de bord</a></li>
-                        <li><a href="?action=manageArticles">Gestion des articles</a></li>
-                        <li><a href="?action=manageComs">Gestion des commentaires</a></li>
-                        <li><a href="?action=listPosts" target="_blank">Retour au site</a></li>
-
-                    </ul>
-                    <ul class="navbar-nav right">
-                        <li class="left"><a href="?action=decoAdmin"><?= ucfirst($_SESSION['user']->logName()) ?> (se déconnecter)</a></li>
-                    </ul>
-                </nav>
-            </header>
-        </div>
-    </div>
-
-    <?= $content ?>
+        <?= $content ?>
 
 
 
@@ -63,6 +65,6 @@
     <!--<script type="text/javascript" src="../js/ajax.js"></script>
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <script type="text/javascript" src=""></script>-->
-</body>
+    </body>
 
 </html>
--- view/frontend/template.php
+++ view/frontend/template.php
@@ -1,36 +1,40 @@
 <!DOCTYPE html>
 <html lang="fr">
 
-<head>
-    <title>
-        <?= $title ?>
-    </title>
+    <head>
+        <title>
+            <?= $title ?>
+        </title>
+        
+        <!--    Pas d'indexation jusqu'a validation client-->
+        <meta name="robots" content="noindex">
+        <meta name="googlebot" content="noindex">
 
 
-    <!-- Compiled and minified MAterialize CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+        <!-- Compiled and minified MAterialize CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
 
-    <!-- CSS Perso-->
-    <link rel="stylesheet" type="text/css" href="public/css/style.css" />
+        <!-- CSS Perso-->
+        <link rel="stylesheet" type="text/css" href="public/css/style.css" />
 
-    <!-- Icons-->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!-- Icons-->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
-    <!--Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Lora|Montez" rel="stylesheet">
+        <!--Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Lora|Montez" rel="stylesheet">
 
-    <!-- Encodage-->
-    <meta charset="utf-8" />
+        <!-- Encodage-->
+        <meta charset="utf-8" />
 
-    <!-- Echelle Mobile -->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-</head>
+        <!-- Echelle Mobile -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    </head>
 
-<body class="grey lighten-3">
+    <body class="grey lighten-3">
 
-    <header class="">
+        <header class="">
 
-        <nav class="navbar-fixed blue-grey darken-4">
+            <nav class="navbar-fixed blue-grey darken-4">
 
                 <a id="logo" href="index.php" class="left">Jean Forteroche</a>
 
@@ -42,32 +46,32 @@
                 <a href="#" data-target="slide-out" class="right button-collapse sidenav-trigger"><i class="material-icons">menu</i></a>
 
 
-        </nav>
-        <ul id="slide-out" class="side-nav">
-            <li><a href="index.php">Accueil</a></li>
-            <li><a href="?action=bio">Biographie</a></li>
-            <li><a href="?action=contact">Contact</a></li>
-        </ul>
+            </nav>
+            <ul id="slide-out" class="side-nav">
+                <li><a href="index.php">Accueil</a></li>
+                <li><a href="?action=bio">Biographie</a></li>
+                <li><a href="?action=contact">Contact</a></li>
+            </ul>
 
 
-    </header>
+        </header>
 
-    <h1 class="titreBlog  light-blue-text text-lighten-5 z-depth-2">Billet simple pour l'Alaska</h1>
-    <section id="content">
-    <?= $content ?>
-    </section>
-    <footer class="blue-grey darken-4">
+        <h1 class="titreBlog  light-blue-text text-lighten-5 z-depth-2">Billet simple pour l'Alaska</h1>
+        <section id="content">
+            <?= $content ?>
+        </section>
+        <footer class="blue-grey darken-4">
 
-        <div class="container">
-            <p><span class="right card grey"><a class="white-text" href="?action=adminConnect">ADMINISTRATION </a></span> </p>
-        </div>
-    </footer>
+            <div class="container">
+                <p><span class="right card grey"><a class="white-text" href="?action=adminConnect">ADMINISTRATION </a></span> </p>
+            </div>
+        </footer>
 
         <!--<script type="text/javascript" src="../js/ajax.js"></script>-->
-    <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-    <!-- Compiled and minified JavaScript Materialize-->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
-    <script type="text/javascript" src="public/js/script.js"></script>
-</body>
+        <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+        <!-- Compiled and minified JavaScript Materialize-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
+        <script type="text/javascript" src="public/js/script.js"></script>
+    </body>
 
 </html>
